4aa1f7a0f046e0566196bf3c46cef4426136b739
Klesev Dimitrij <dimitrij.klesev@gmail.com>
2025-01-13 00:06:09 +0100

    feat(global-bottom.vue): add 'empty' layout to footer visibility conditions to hide footer in empty layout
    feat(layouts/empty.vue): create new empty layout component to support slides without content
    
    style(layout.css): update cover layout background and text colors for improved readability
    
    style(layout.css): adjust logo and footer positioning for better alignment

--- global-bottom.vue
+++ global-bottom.vue
@@ -1,16 +1,16 @@
 <template>
     <footer 
-        v-if="$nav.currentLayout !== ('cover' && 'lead')" 
+        v-if="$nav.currentLayout !== ('cover' && 'lead' && 'empty')" 
         class="footer footer-left">
         {{ $slidev.configs.date }}
     </footer>
     <footer 
-        v-if="$nav.currentLayout !== ('cover' && 'lead')" 
+        v-if="$nav.currentLayout !== ('cover' && 'lead' && 'empty')" 
         class="footer footer-center">
         eBPF Vienna
     </footer>
     <footer 
-        v-if="$nav.currentLayout !== ('cover' && 'lead')" 
+        v-if="$nav.currentLayout !== ('cover' && 'lead' && 'empty')" 
         class="footer footer-right">
         {{ $nav.currentPage }}
     </footer>
---
+++ layouts/empty.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="slidev-layout empty">
+        <slot />
+    </div>
+  </template>
+  
