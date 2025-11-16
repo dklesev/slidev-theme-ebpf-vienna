b733ad61521d623430e145c7ed58cebfd461ec8b
Klesev Dimitrij <dimitrij.klesev@gmail.com>
2025-11-16 21:31:32 +0100

    refactor: simplify footer and logo visibility logic by removing computed properties
    Signed-off-by: Klesev Dimitrij <dimitrij.klesev@gmail.com>

--- global-bottom.vue
+++ global-bottom.vue
@@ -1,24 +1,17 @@
-<script setup lang="ts">
-import { computed } from 'vue'
-import { useNav } from '@slidev/client'
-
-const nav = useNav()
-
-const showFooter = computed(() => 
-  !['cover', 'lead', 'empty'].includes(nav.currentLayout.value)
-)
-</script>
-
 <template>
-  <template v-if="showFooter">
-    <footer class="footer footer-left">
-      {{ $slidev.configs.date }}
+    <footer 
+        v-if="!['cover', 'lead', 'empty'].includes($nav.currentLayout)" 
+        class="footer footer-left">
+        {{ $slidev.configs.date }}
     </footer>
-    <footer class="footer footer-center">
-      eBPF Vienna
+    <footer 
+        v-if="!['cover', 'lead', 'empty'].includes($nav.currentLayout)" 
+        class="footer footer-center">
+        eBPF Vienna
     </footer>
-    <footer class="footer footer-right">
-      {{ nav.currentPage }}
+    <footer 
+        v-if="!['cover', 'lead', 'empty'].includes($nav.currentLayout)" 
+        class="footer footer-right">
+        {{ $nav.currentPage }}
     </footer>
-  </template>
 </template>
--- global-top.vue
+++ global-top.vue
@@ -1,18 +1,7 @@
-<script setup lang="ts">
-import { computed } from 'vue'
-import { useNav } from '@slidev/client'
-
-const nav = useNav()
-
-const showLogo = computed(() => 
-  !['cover', 'lead', 'empty'].includes(nav.currentLayout.value)
-)
-</script>
-
 <template>
   <SlidevSpotlight />
   <img 
-    v-if="showLogo" 
+    v-if="!['cover', 'lead', 'empty'].includes($nav.currentLayout)" 
     src="https://ebpf.foundation/wp-content/uploads/sites/9/2023/03/ebpf-icon-292x300.png" 
     class="logo" 
     alt="eBPF logo"
