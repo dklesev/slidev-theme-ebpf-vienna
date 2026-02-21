cf27abbded7eab35b96dc33704c8476a4de29232
Klesev Dimitrij <dimitrij.klesev@gmail.com>
2026-02-22 00:42:45 +0100

    feat(layout): add VHS layout and enhance footer visibility
    Signed-off-by: Klesev Dimitrij <dimitrij.klesev@gmail.com>

--- global-bottom.vue
+++ global-bottom.vue
@@ -1,16 +1,16 @@
 <template>
     <footer 
-        v-if="!['cover', 'lead', 'empty'].includes($nav.currentLayout)" 
+        v-if="!['cover', 'lead', 'empty', 'vhs'].includes($nav.currentLayout)" 
         class="footer footer-left">
         {{ $slidev.configs.date }}
     </footer>
     <footer 
-        v-if="!['cover', 'lead', 'empty'].includes($nav.currentLayout)" 
+        v-if="!['cover', 'lead', 'empty', 'vhs'].includes($nav.currentLayout)" 
         class="footer footer-center">
         eBPF Vienna
     </footer>
     <footer 
-        v-if="!['cover', 'lead', 'empty'].includes($nav.currentLayout)" 
+        v-if="!['cover', 'lead', 'empty', 'vhs'].includes($nav.currentLayout)" 
         class="footer footer-right">
         {{ $nav.currentPage }}
     </footer>
---
+++ layouts/vhs.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="slidev-layout vhs">
+    <slot />
+  </div>
+</template>
